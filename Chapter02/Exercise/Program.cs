ca5f39eecc736e5fc5ac179bea58c036569c1b91
33287WataruTsuchido <33287@ojs.ac.jp>
2022-05-10 14:50:18 +0900

    問題2.1.1 問題2.1.2

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+            //2.1.3
+        private List<Song> _song;
+        
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] args) {
+
         }
     }
 }
--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -5,13 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise {
-    
+
     public class Song {
         //歌のタイトル
-        public string title { get; set; }
+        public string Title { get; set; }
         //アーティスト名
         public string ArtistName { get; set; }
         //演奏時間、単位は秒
         public int Length { get; set; }
+
+        //引数付きインストラクタ
+        public Song(string title, string artistname, int length) {
+            this.Title = title;
+            this.ArtistName = artistname;
+            this.Length = length;
+        }
     }
 }
