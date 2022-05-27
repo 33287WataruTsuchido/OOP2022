59d992c4fab794144129c920065821675c86c4e7
33287WataruTsuchido <33287@ojs.ac.jp>
2022-05-27 13:39:27 +0900

    ５－２ 演習問題

--- Chapter05/Exercise02/Program.cs
+++ Chapter05/Exercise02/Program.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-        
+            var line = Console.ReadLine();
+            int num; //整数を入れる数値を入力
+
+            //p126
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine("{0:#:#}", num);//整数への変換成功
+            }
+            else {
+                Console.WriteLine("数値文字列でありません");//変換失敗
+            
+
+            }
 
             
         }
---
+++ Chapter05/Exercise03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise03_1(text);
+            Console.WriteLine("----------");
+
+            Exercise03_2(text);
+            Console.WriteLine("----------");
+
+            Exercise03_3(text);
+            Console.WriteLine("----------");
+
+            Exercise03_4(text);
+            Console.WriteLine("----------");
+
+            Exercise03_5(text);
+            Console.WriteLine("----------");
+        }
+
+        private static void Exercise03_1(string text) {
+            
+        }
+
+        private static void Exercise03_2(string text) {
+           
+        }
+
+        private static void Exercise03_3(string text) {
+
+        }
+        private static void Exercise03_4(string text) {
+          
+        }
+
+        private static void Exercise03_5(string text) {
+            
+        }
+    }
+}
