4750dd9889e03756b5b6a76eb828adb6f47696cc
33287WataruTsuchido <33287@ojs.ac.jp>
2022-06-14 14:30:05 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,42 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
 
 
 
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
 
             
-        }
 
-        //メソッドの概要： 
+
+
+
+    }
+
+        //メソッドの概要： 点数を集計する
         public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<String, int>();
+            foreach (var sale in _sales) {
+              if (dict.ContainsKey(sale.ShopName))
+                dict[sale.ShopName] += sale.Amount;
 
+             else
 
-
-
-
-            
+                dict[sale.ShopName] = sale.Amount;
         }
+
+        return dict;
+    }
+
+
+
+
+
+}
     }
 }
--- Test202206/TestProject/Test01/Student.cs
+++ Test202206/TestProject/Test01/Student.cs
@@ -2,6 +2,15 @@
     class Student {
 
 
+        //学生の名前
+        public string Name { get; set; }  //プロパティ（自動実装プロパティ）
+
+        //科目名
+        public string Subject{ get; set; }
+
+        //点数
+        public int Score { get; set; }
+
 
     }
 }
