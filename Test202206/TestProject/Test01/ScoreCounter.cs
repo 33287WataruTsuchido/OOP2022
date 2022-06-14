75f54d29ad1828ad962a2475971df47f14810420
33287WataruTsuchido <33287@ojs.ac.jp>
2022-06-14 15:08:13 +0900

    統一テスト　設問1(Test01)完成

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -14,35 +14,37 @@ namespace Test01 {
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要：集計データを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
+            List<Student> scores = new List<Student>();
+            string[]lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                Student score = new Student {
+                    Name = items[1],
+                    Subject = items[2],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(score);
+            }
+             return scores;
 
-            
+        }
 
-
-
-
-    }
-
-        //メソッドの概要： 点数を集計する
+        //メソッドの概要： 点数を集計して求める
         public IDictionary<string, int> GetPerStudentScore() {
-            var dict = new Dictionary<String, int>();
-            foreach (var sale in _sales) {
-              if (dict.ContainsKey(sale.ShopName))
-                dict[sale.ShopName] += sale.Amount;
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+              if (dict.ContainsKey(score.Name))
+                dict[score.Name] += score.Score;
 
              else
 
-                dict[sale.ShopName] = sale.Amount;
+                dict[score.Name] = score.Score;
         }
 
         return dict;
     }
 
-
-
-
-
-}
     }
 }
