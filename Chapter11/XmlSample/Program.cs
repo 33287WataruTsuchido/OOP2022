4818583ada53bb2829d97527e66347a93b6ccf71
33287WataruTsuchido <33287@ojs.ac.jp>
2022-06-28 13:35:21 +0900

    RSS取得コンソール

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace XmlSample {
+    class Program {
+        static void Main(string[] args) {
+            using (var wc = new WebClient()) {
+
+
+                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/number/all.xml");
+
+                var xdoc = XDocument.Load(stream);
+                var xnews = xdoc.Root.Descendants("item").Select(x=>x.Element("title"));
+
+                foreach (var data in xnews) {
+
+                    Console.WriteLine(data);
+                }
+
+            }
+        }     
+        
+    }
+}
