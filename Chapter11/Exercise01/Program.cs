44baea61b08458f4ff736d7ede15088717b1b717
33287WataruTsuchido <33287@ojs.ac.jp>
2022-07-04 11:52:20 +0900

    p299 Exercise1_1 完成

---
+++ Chapter11/Exercise01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+
+            var xdoc = XDocument.Load("Sample.xml");
+            var novelists = xdoc.Root.Elements()
+                                    .Select(x => new {
+                                        Name = (string)x.Element("name"),
+                                        Teammember = (int)x.Element("teammembers")
+                                    });   
+                                        
+
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} {1}人",novelist.Name,novelist.Teammember);
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+           
+            var xdoc = XDocument.Load("Sample.xml");
+            var novelists = xdoc.Root.Elements()
+                                   .OrderBy(x =>(string)(x.Element("ballsport").Attribute("firstplayed")));
+            
+            foreach (var item in novelists) {
+                var xfirstplayed = item.Element("firstplayed");
+                var xballsport = item.Element("ballsport");
+                
+                Console.WriteLine("{0}{1}",xballsport.Value,xfirstplayed.Value);
+
+
+            }
+
+        }
+
+        private static void Exercise1_3(string file) {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -9,21 +9,19 @@ using System.Net;
 namespace XmlSample {
     class Program {
         static void Main(string[] args) {
-            using (var wc = new WebClient()) {
 
 
-                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/number/all.xml");
-
-                var xdoc = XDocument.Load(stream);
-                var xnews = xdoc.Root.Descendants("item").Select(x=>x.Element("title"));
-
-                foreach (var data in xnews) {
-
-                    Console.WriteLine(data);
-                }
-
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
-        }     
-        
+        }
     }
 }
