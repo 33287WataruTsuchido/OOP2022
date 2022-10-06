1d1e913ebd64948ae8769ced7556b5a2090f3634
33287WataruTsuchido <33287@ojs.ac.jp>
2022-10-06 15:05:35 +0900

    15章演習問題　 2、５ 完成

--- Chapter15/Exercise01/Program.cs
+++ Chapter15/Exercise01/Program.cs
@@ -25,16 +25,52 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2() {
+            var max = Library.Books
+                           .Max(b => b.Price);
+            var book = Library.Books.First((b => b.Price == max));
+
+            Console.WriteLine(book);
+
         }
+
 
         private static void Exercise1_3() {
-        }
+           // var query = Library.Books
+                                 //  .OrderBy(x => x.PublishedYear)
+                                 //  .Select();
+   
 
+          //  foreach (var book in query) {
+            //    Console.WriteLine($"");
+
+          //  }
+        }
         private static void Exercise1_4() {
+          //  var selected = Library.Books
+                                  // .Join(Library.Categories,
+                                  // book => book.CategoryId,
+                                  // (book,category) =
+                                  
+                                   
+           // foreach (var book in selected) {
+             //   Console.WriteLine($"");
+
+           // }
         }
 
         private static void Exercise1_5() {
-        }
+            var query = Library.Books
+                                .Where(b => b.PublishedYear == 2016)
+                                .Join(
+                                         Library.Categories,
+                                          book => book.CategoryId,
+                                         catergory => catergory.Id,
+                                         (book, category) => category.Name)
+                                .Distinct();
+            foreach (var name in query) {
+                Console.WriteLine(name);
+            }
+    }
 
         private static void Exercise1_6() {
         }
