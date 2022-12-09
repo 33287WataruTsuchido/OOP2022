9523a8d9f7ba5410779c7049aba2a773d4f259c4
33287WataruTsuchido <33287@ojs.ac.jp>
2022-12-09 15:15:28 +0900

    wasd

---
+++ MovieWPF/MovieApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace MovieApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btSearch_Click(object sender, RoutedEventArgs e)
+        {
+            {
+
+
+                this.Hide();
+                Window1 f2 = new Window1();
+                f2.Show();
+
+
+                var wc = new WebClient()
+                {
+                    Encoding = Encoding.UTF8
+                };
+
+                var url = " ";
+                url = wc.DownloadString("https://api.themoviedb.org/3/movie/550?api_key=6e89cd650ebfbdcb78abf5d855137280");
+
+                var json = JsonConvert.DeserializeObject<Rootobject>(url);
+
+
+               
+
+
+
+
+            }
+        }
+
+        private void btHistory_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Hide();
+            Window2 f2 = new Window2();
+            f2.Show();
+
+        }
+    }
+}
---
+++ MovieWPF/MovieApp/tmfb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieApp
+{
+ 
+        public class Rootobject
+        {
+            public string  original_title { get; set; }
+            public string poster_path { get; set; }          
+            public string release_date { get; set; }
+            public string overview { get; set; }
+            public string genres { get; set; }
+            public string revenue { get; set; }
+
+    }
+
+ 
+}
+
